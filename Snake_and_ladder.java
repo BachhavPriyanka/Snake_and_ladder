269770013662d6ef76f1a868aa3dc0c30a292148
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-02 11:42:37 +0530

    player playing snake and ladder

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -1,13 +1,41 @@
 package day4;
 
+import java.util.Scanner;
+
 public class Snake_and_ladder {
 
 	public static void main(String[] args) {
-		int startPos = 0;  
-        int dieRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
-        
-        System.out.println("Starting Position is :" + startPos);
-        System.out.println("Dice Roll Number :" + dieRoll);
+        int startPos = 0;
+        System.out.println("Enter name: ");
+        Scanner sc = new Scanner(System.in);
+        String Player_Name = sc.next();
+
+        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Dice Roll number :" + dieRoll);
+
+        int checkOption = (int) (Math.floor(Math.random() * 10) % 3);
+
+        switch (checkOption)
+
+        {
+            case 0:
+                System.out.println("No Play");
+                startPos += 0;
+                break;
+
+            case 1:
+                System.out.println("Ladder");
+                startPos += dieRoll;
+                break;
+
+            default:
+                System.out.println("Snake");
+                startPos -= dieRoll;
+
+                if (startPos < 0)
+                    startPos = 0;
+        }
+        System.out.println( Player_Name + " is on Position :" + startPos);
 	}
 
 }
