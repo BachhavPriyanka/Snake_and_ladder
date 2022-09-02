476ec6d869759a9154b9fc3ff9b561307d66e0fb
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-02 11:31:23 +0530

    Random dice rolling 1 to 6

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -3,8 +3,11 @@ package day4;
 public class Snake_and_ladder {
 
 	public static void main(String[] args) {
-		int startPos = 0;
+		int startPos = 0;  
+        int dieRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+        
         System.out.println("Starting Position is :" + startPos);
+        System.out.println("Dice Roll Number :" + dieRoll);
 	}
 
 }
