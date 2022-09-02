8a211a56af152578200f0d3e5d4d4622c7bf1f2e
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-02 11:19:28 +0530

    Single player playing from start position

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -3,8 +3,8 @@ package day4;
 public class Snake_and_ladder {
 
 	public static void main(String[] args) {
-		  System.out.println("Welcome To Snake And Ladder Simulator");
-
+		int startPos = 0;
+        System.out.println("Starting Position is :" + startPos);
 	}
 
 }
