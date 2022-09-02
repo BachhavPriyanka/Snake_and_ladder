366fa6d2da3cd819cbb9f7ef2c438c1ba4ff4272
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-02 12:28:14 +0530

    number of

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -6,45 +6,48 @@ public class Snake_and_ladder {
 	public static final int Winning_Position = 100;
 	
 	public static void main(String[] args) {
-		int startPosition = 0;
-        System.out.println("Enter your name: ");
-        Scanner sc = new Scanner(System.in);
-        String Player_Name = sc.next();
+		 int startPosition = 0;
+	        int rollCounter = 0;
+	        System.out.println("Enter your name: ");
+	        Scanner sc = new Scanner(System.in);
+	        String Player_Name = sc.next();
 
-        while (Winning_Position > startPosition) {
+	        while (Winning_Position > startPosition) {
 
-            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-            int needToWin = Winning_Position - startPosition;
-            int checkOption = (int) (Math.floor(Math.random() * 10) % 3);
+	            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+	            rollCounter ++;
+	            int needToWin = Winning_Position - startPosition;
+	            int checkOption = (int) (Math.floor(Math.random() * 10) % 3);
 
-            if(needToWin >= dieRoll) {
+	            if(needToWin >= dieRoll) {
 
-                switch (checkOption) {
-                    case 0:
-                        System.out.println("No Play");
-                        startPosition += 0;
-                        break;
+	                switch (checkOption) {
+	                    case 0:
 
-                    case 1:
-                        System.out.println("Ladder:" + " + " + dieRoll);
-                        startPosition += dieRoll;
-                        break;
+	                        startPosition += 0;
+	                        break;
 
-                    default:
-                        System.out.println("Snake :" + " - " + dieRoll);
-                        startPosition -= dieRoll;
+	                    case 1:
 
-                        if (startPosition < 0) {
-                            System.out.println("Start Again");
-                            startPosition = 0;
-                        }
+	                        startPosition += dieRoll;
+	                        break;
 
-                }
-            }
-        }
+	                    default:
 
-        System.out.println(Player_Name + " Reached the Winning Position :" + startPosition);
+	                        startPosition -= dieRoll;
 
+	                        if (startPosition < 0) {
+	                            System.out.println("Start Again");
+	                            startPosition = 0;
+	                        }
+
+	                }
+	            }
+	            System.out.println("Player Current Position" + startPosition);
+	        }
+
+	        System.out.println(Player_Name + " Reached the Winning Position :" + startPosition);
+	        System.out.println("Dice Rolled :"+rollCounter+" Times");
 	}
 
 }
